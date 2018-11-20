0e4cc4fbd7da996f4e4073ad5a4a783fb18a094a
cbtsao47 <cbtsao47@gmail.com>
2018-11-20 13:09:51 -0600

    add mecomponent

--- day1/src/App.js
+++ day1/src/App.js
@@ -2,21 +2,23 @@ import React from "react";
 import "./App.css";
 
 const App = () => {
+  const MeComponent = props => {
+    return (
+      <div>
+        <h1>{props.me.name}</h1>
+        <p>{props.me.age}</p>
+        <p>{props.me.location}</p>
+      </div>
+    );
+  };
+  const myObj = {
+    name: "Ben",
+    age: 31,
+    location: "dallas"
+  };
   return (
     <div>
-      <h3>Welcome to React Social Card!</h3>
-      <p>
-        Begin by exploring the `components` directory. You'll notice we have a
-        few files that we've already included in there to get you started right
-        away building components. You'll need to make sure you include your
-        components that you build in this file to watch your app come to life
-      </p>
-      <p>
-        <strong>
-          Don't forget to `default export` your components and import them here
-          inside of this file in order to make them work.
-        </strong>
-      </p>
+      <MeComponent me={myObj} />
     </div>
   );
 };
