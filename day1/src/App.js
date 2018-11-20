d9b8532593cfede1d9bad91b4c98e6a8a7878f2a
cbtsao47 <cbtsao47@gmail.com>
2018-11-20 13:49:58 -0600

    add bart profile with mapping and destructuring

--- day1/src/App.js
+++ day1/src/App.js
@@ -1,26 +1,32 @@
 import React from "react";
 import "./App.css";
 
-const App = () => {
-  const MeComponent = props => {
-    return (
-      <div>
-        <h1>{props.me.name}</h1>
-        <p>{props.me.age}</p>
-        <p>{props.me.location}</p>
-      </div>
-    );
-  };
-  const myObj = {
-    name: "Ben",
-    age: 31,
-    location: "dallas"
-  };
+const Bart = props => {
+  const { bart } = props;
   return (
-    <div>
-      <MeComponent me={myObj} />
-    </div>
+    <React.Fragment>
+      <h3>{bart.name}</h3>
+      <p>Identity:{bart.identiy}</p>
+      <p>Age:{bart.age}</p>
+      <ul>
+        {bart.family.map((member, i) => (
+          <li key={i}>{member}</li>
+        ))}
+      </ul>
+    </React.Fragment>
   );
+};
+
+const App = () => {
+  const BartProfile = {
+    name: "Bartholomew Jo-Jo Simpson",
+    identiy: "the troublemaker of the simpson family.",
+    age: 10,
+    family: ["homer", "marge", "lisa", "maggie", "abe"],
+    best_friend: "milhouse van houten",
+    catchphrases: ["aye carumba!", "eat my shorts", "i didn't do it"]
+  };
+  return <Bart bart={BartProfile} />;
 };
 
 export default App;
